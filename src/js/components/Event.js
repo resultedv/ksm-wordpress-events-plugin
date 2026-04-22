78354440f3ad7c091df3c50d7dc97c905ae419c9
Josedaniel Huereca <josedanielhuerecauriegas@gmail.com>
2026-04-22 14:22:01 +0200

    Modify the html of the event dashboard, created a new css file to override the other file in the plugin

--- src/js/components/Event.js
+++ src/js/components/Event.js
@@ -11,28 +11,47 @@ export default function Event({event}) {
 	return (
 		<li key={event.id} className="ksm-event">
 			<div className="content-container">
-				{event.ksm?.date && <div className="datetime" {...escapeHTML( event.ksm?.date )} />}
-				<h2 {...escapeHTML( event.title.rendered )} />
-				<div className="type-institute" {...escapeHTML( [event.acf?.type, event.acf?.institute].filter(Boolean).join(event.ksm?.sep) )} />
-				<blockquote {...escapeHTML( event.content.rendered )} />
-				<div className="featured-image" {...escapeHTML( event.ksm?.featured_image )} />
-				{event.ksm?.registration_link && (
-					<button>
-						<a href={event.ksm?.registration_link} target="_blank">
-							Zur Anmeldung
-						</a>
-					</button>
-				)}
-			</div>
-			<ul className="info-container">
-				{event.ksm?.info && ( Object.entries(event.ksm?.info).map(([type, value]) => (
-					<li key={type}>
-						<strong>{type}</strong>
-						<br />
-						<span {... escapeHTML( value )} />
-					</li>
-				) ) )}
-			</ul>
+
+  {event.ksm?.date && (
+    <div className="datetime" {...escapeHTML(event.ksm?.date)} />
+  )}
+
+  <h2 {...escapeHTML(event.title.rendered)} />
+
+  <div className="type-institute"
+    {...escapeHTML(
+      [event.acf?.type, event.acf?.institute]
+        .filter(Boolean)
+        .join(event.ksm?.sep)
+    )}
+  />
+
+  <blockquote {...escapeHTML(event.content.rendered)} />
+
+  <div className="featured-image"
+    {...escapeHTML(event.ksm?.featured_image)}
+  />
+
+  {/* 🔥 MOVE INFO INSIDE */}
+  <div className="info-row">
+    {event.ksm?.info &&
+      Object.entries(event.ksm.info).map(([type, value]) => (
+        <span key={type}>
+          <strong>{type}:</strong>{" "}
+          <span {...escapeHTML(value)} />
+        </span>
+      ))}
+  </div>
+
+  {event.ksm?.registration_link && (
+    <button>
+      <a href={event.ksm.registration_link} target="_blank">
+        Zur Anmeldung
+      </a>
+    </button>
+  )}
+
+</div>
 		</li>
 	);
 }
